161ad5c9eb16e3c2d7822f163fe90331ebc7d4a9
米谷洸亮 <ne201218@senshu-u.jp>
2022-06-26 08:05:19 +0000

    User詳細

--- db/migrate/20220617062013_create_messages.rb
+++ db/migrate/20220617062013_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.string :text
-      t.timestamps :date
+      t.datetime :date
       t.references :user
       t.references :room
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_030450) do
+ActiveRecord::Schema.define(version: 2022_06_17_063121) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.datetime "date"
+    t.integer "user_id"
+    t.integer "room_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
+    t.index ["user_id"], name: "index_user_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
