dee628e3a4d2eadd2e0a60fd7b6987e51bcbda0d
米谷洸亮 <ne201218@senshu-u.jp>
2022-09-16 07:26:57 +0000

    新規登録の流れ完成(仮)

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,5 @@
 class AccountsController < ApplicationController
-  before_action :login_required, except: [:new, :create]
+  before_action :login_required, except: [:new, :create, :update]
 
   def show
     @user=current_user
@@ -28,12 +28,22 @@ class AccountsController < ApplicationController
   end
 
   def new
-    @user = User.new
-    @timetable = @user.timetables.new
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      if @user&.authenticate(params[:password])
+        @timetable = @user.timetables.new
+      else
+        @user= nil
+        redirect_to :new_account, notice: "パスワードが違います。"
+      end
+    else
+      render "new"
+    end
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.find_by(email: params[:user][:email])
+    @user.assign_attributes(user_params)
     tt = @user.timetables.new(params[:user][:timetable])
     if @user.save && tt.save
       cookies.signed[:user_id] = { value: @user.id }
@@ -45,6 +55,10 @@ class AccountsController < ApplicationController
     params.require(:account).permit(:name, :sex, :birthplace, :undergraduate, :comment, :image, :twitter, :instagram, :tiktok, timetables_attributes: [:timetable, :destroy, :id])
   end
   private def user_params
-    params.require(:user).permit(:name, :sex, :birthplace, :undergraduate, :comment, :image, :twitter, :instagram, :tiktok, :password)
+    params.require(:user).permit(:name, :sex, :birthplace, :undergraduate, :comment, :image, :twitter, :instagram, :tiktok, :email)
+  end
+
+  private def register_params
+    params.require(:user).permit(:email, :password)
   end
 end
