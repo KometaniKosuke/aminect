f7457552b66daaa79e5aa3b309d2f3a00c627c8a
KometaniKosuke <ne201218@senshu-u.jp>
2022-10-09 01:57:29 +0900

    投稿画像削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,24 +10,29 @@ class PostsController < ApplicationController
       render "new"
     end
   end
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.assign_attributes(post_params)
-    if @post.save
-      redirect_to :account, notice: "投稿を更新しました。"
-    else
-      render "edit"
-    end
-  end
+  # def edit
+  #   @post = Post.find(params[:id])
+  # end
+  #
+  # def update
+  #   @post = Post.find(params[:id])
+  #   @post.assign_attributes(post_params)
+  #   if @post.save
+  #     redirect_to :account, notice: "投稿を更新しました。"
+  #   else
+  #     render "edit"
+  #   end
+  # end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    path = Dir.pwd+"/public/uploads/post/post_img/#{params[:id]}/"
+    FileUtils.remove_dir(path)
+    redirect_to :account, notice: "投稿を削除しました"
   end
 
-  private def post_params
-    params.require(:post).permit(:text, :img)
- end
+ #  private def post_params
+ #    params.require(:post).permit(:text, :post_img)
+ # end
 end
